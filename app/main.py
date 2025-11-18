60afabf965de16b8802bf765b71c6352e65d0c38
Karan Arora <ar0ra.karan7890@gmail.com>
2025-11-17 22:08:02 -0500

    Implement exit

--- app/main.py
+++ app/main.py
@@ -5,7 +5,9 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     while True:
         userInput = input("$ ")
-        sys.stdout.write(f"{userInput}: command not found \n")
+        sys.exit() if userInput == "exit 0" else sys.stdout.write(f"{userInput}: command not found \n")
+        
+
     pass
 
 
