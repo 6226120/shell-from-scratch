9c13cf1e84a8c3e39628e98ddfae1bddb70254b4
Karan Arora <ar0ra.karan7890@gmail.com>
2025-11-19 21:36:35 -0500

    Refactor my code to be more cleaner and change bad coding habbits

--- app/main.py
+++ app/main.py
@@ -2,19 +2,26 @@ import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
+
     while True:
         userInput = input("$ ")
-        if "echo" in userInput:
-            userInput = userInput.replace("echo ", "")
-            sys.stdout.write(f"{userInput} \n")
-        elif "exit" in userInput:
+        
+        # We want to tokenize the input so we can differentiate the command from the args
+        userInputTokens = userInput.split()
+        command = userInputTokens[0]
+        arg = userInputTokens[1:]
+
+        # We are checking to see what the firs token is and responding accordingly 
+        if "echo" == command:
+            sys.stdout.write(f"{arg}\n")
+        elif "exit" == command:
             sys.exit()
         else:
-            sys.stdout.write(f"{userInput}: command not found \n")
-        
+            command_not_found(userInput)
+    
 
-    pass
+def command_not_found(userInput):
+    sys.stdout.write(f"{userInput}: command not found \n")
 
 
 if __name__ == "__main__":
